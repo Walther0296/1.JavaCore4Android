073b410ad743601c4d53b773d14d507d82e06a01
Walther0296 <sau100@inbox.ru>
2025-04-06 10:15:06 +0300

    Initial Commit

--- src/main/l1/Introduction/Main.java
+++ src/main/l1/Introduction/Main.java
@@ -1,3 +1,5 @@
+package main.l1.Introduction;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
