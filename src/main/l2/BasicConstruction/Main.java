dce89f6ea3dbebd22be534f888f4d0edad40563f
Walther0296 <sau100@inbox.ru>
2025-04-06 13:27:24 +0300

    HW2 Task2

--- src/main/l2/BasicConstruction/Main.java
+++ src/main/l2/BasicConstruction/Main.java
@@ -3,6 +3,7 @@ package main.l2.BasicConstruction;
 public class Main {
     public static void main(String[] args) {
         // check();
+        check2();
     }
 
     // 1. Написать метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит в пределах от 10 до 20 (
@@ -15,4 +16,16 @@ public class Main {
         int sum = a + b;
         System.out.println(sum <= 20 & sum >= 10);
     }
+
+    // 2. Написать метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль,
+    //   положительное ли число передали или отрицательное. Замечание: ноль считаем положительным числом.
+    public static void check2() {
+        int a = -9;
+        if (a < 0) {
+            System.out.println("Число отрицательное");
+        } else {
+            System.out.println("Число положительное");
+        }
+    }
+
 }
