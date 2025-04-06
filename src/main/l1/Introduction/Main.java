78ef46699e4a45155370216c01bd57960e6a2499
Walther0296 <sau100@inbox.ru>
2025-04-06 10:28:23 +0300

    Task3 complete

--- src/main/l1/Introduction/Main.java
+++ src/main/l1/Introduction/Main.java
@@ -1,17 +1,36 @@
 package main.l1.Introduction;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        printThreeWords();
+        checkSumSign();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
+
+
+    //2. Создан метод printThreeWords(), который при вызове выводит в столбец три слова:
+    //   Orange, Banana, Apple.
+    public static void printThreeWords() {
+        System.out.printf("Orange\n" +
+                "Banana\n" +
+                "Apple");
+        System.out.println();
+    }
+
+    ;
+
+    //Создайте метод checkSumSign(), в теле которого объявите две int переменные a и b,
+    //   и инициализируйте их любыми значениями, которыми захотите. Далее метод должен просуммировать эти переменные, и если
+    //   их сумма больше или равна 0, то вывести в консоль сообщение “Сумма положительная”, в противном случае - “Сумма
+    //   отрицательная”;
+    public static void checkSumSign() {
+        int a = 9;
+        int b = 3;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else System.out.println("Сумма отрицательная");
+    };
+
+
 }
