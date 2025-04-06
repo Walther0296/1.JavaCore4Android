5632c511339d00bf0cb44f214adac222483ef500
Walther0296 <sau100@inbox.ru>
2025-04-06 10:33:48 +0300

    Task3 complete

--- src/main/l1/Introduction/Main.java
+++ src/main/l1/Introduction/Main.java
@@ -3,8 +3,9 @@ package main.l1.Introduction;
 
 public class Main {
     public static void main(String[] args) {
-        printThreeWords();
-        checkSumSign();
+        //printThreeWords();
+        //checkSumSign();
+        printColor();
 
     }
 
@@ -20,7 +21,7 @@ public class Main {
 
     ;
 
-    //Создайте метод checkSumSign(), в теле которого объявите две int переменные a и b,
+    //3. Создайте метод checkSumSign(), в теле которого объявите две int переменные a и b,
     //   и инициализируйте их любыми значениями, которыми захотите. Далее метод должен просуммировать эти переменные, и если
     //   их сумма больше или равна 0, то вывести в консоль сообщение “Сумма положительная”, в противном случае - “Сумма
     //   отрицательная”;
@@ -30,7 +31,24 @@ public class Main {
         if (a + b >= 0) {
             System.out.println("Сумма положительная");
         } else System.out.println("Сумма отрицательная");
-    };
+    }
 
+    ;
+
+//4. Создайте метод printColor() в теле которого задайте int переменную value и инициализируйте ее любым значением. Если
+//   value меньше 0 (0 включительно), то в консоль метод должен вывести сообщение “Красный”, если лежит в пределах от 0 (0
+//   исключительно) до 100 (100 включительно), то “Желтый”, если больше 100 (100 исключительно) - “Зеленый”;
+
+    public static void printColor() {
+        int value = -130;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value <= 100) {
+            System.out.println("Желтый");
+        } else System.out.println("Зеленый");
+
+    }
+
+    ;
 
 }
