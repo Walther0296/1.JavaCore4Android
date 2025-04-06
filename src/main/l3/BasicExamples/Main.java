d3fbcc4e213b908e742bb8653e12df8bd5a48129
Walther0296 <sau100@inbox.ru>
2025-04-06 18:53:01 +0300

    HW3 Task7

--- src/main/l3/BasicExamples/Main.java
+++ src/main/l3/BasicExamples/Main.java
@@ -1,6 +1,5 @@
 package main.l3.BasicExamples;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Main {
@@ -9,7 +8,9 @@ public class Main {
         // arrHundred();
         // thirdTask();
         // fourthTask(25);
-        fifthTask(4, 4);
+        // fifthTask(4, 4);
+        // sixthTask();
+        // seventhTask(new int[]{3, 5, 3, 2, 6, 2, 1});
     }
 
     // 1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью
@@ -71,15 +72,55 @@ public class Main {
 // и возвращающий одномерный массив типа int
 //   длиной len, каждая ячейка которого равна initialValue;
 
-    private static void fifthTask(int len, int initialValue){
-        int [] arr = new int[len];
-        for (int i = 0; i < arr.length; i++){
-            arr [i] = initialValue;
+    private static void fifthTask(int len, int initialValue) {
+        int[] arr = new int[len];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = initialValue;
         }
         System.out.println(Arrays.toString(arr));
     }
 
-    // 6.* Задать одномерный массив и найти в нем минимальный и максимальный элементы ;
+    // 6.* Задать одномерный массив и найти в нем минимальный и максимальный элементы;
 
+    private static void sixthTask() {
+        int[] arr = {1, 3, 5, 6, 8, 4, 5, 7, 10, 14, 356, 5, 7};
+        int min = arr[0];
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Минимальное число: " + min);
+        System.out.println("Максимальное число: " + max);
+
+    }
+    // 7. ** Написать метод, в который передается не пустой одномерный целочисленный массив,
+    // метод должен вернуть true, если в
+    //   массиве есть место, в котором сумма левой и правой части массива равны.
+    //   **Примеры:
+    //   checkBalance([2, 2, 2, 1, 2, 2, ||| 10, 1]) → true, т.е. 2 + 2 + 2 + 1 + 2 + 2 = 10 + 1
+    //   checkBalance([1, 1, 1, ||| 2, 1]) → true, т.е. 1 + 1 + 1 = 2 + 1
+    //
+    //граница показана символами |||, эти символы в массив не входят и не имеют никакого отношения к ИЛИ.
+
+    private static void seventhTask(int[] arr) {
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
+        }
+        System.out.println(sum % 2 == 0);
+    }
+
+    // 8. *** Написать метод, которому на вход подается одномерный массив и число n (может быть положительным, или
+    //   отрицательным), при этом метод должен сместить все элементы массива на n позиций. Элементы смещаются циклично. Для
+    //   усложнения задачи нельзя пользоваться вспомогательными массивами. Примеры: [ 1, 2, 3 ] при n = 1 (на один
+    //   вправо) -> [ 3, 1, 2 ]; [ 3, 5, 6, 1] при n = -2 (на два влево) -> [ 6, 1, 3, 5 ]. При каком n в какую сторону сдвиг
+    //   можете выбирать сами.
+    //   Если выполнение задач вызывает трудности, можете обратиться к последней странице методического пособия. Для задач
+    //   со * не нужно искать решение в интернете, иначе нет смысла их выполнять.
 
 }
