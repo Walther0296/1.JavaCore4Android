c871da0e35b804c8a2e5f91167ccbb7544674566
Walther0296 <sau100@inbox.ru>
2025-04-06 16:49:37 +0300

    HW3 Task1

--- src/main/l3/BasicExamples/Main.java
+++ src/main/l3/BasicExamples/Main.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        changeArray();
+        // changeArray();
+        arrHundred();
     }
 
     // 1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью
@@ -18,6 +19,15 @@ public class Main {
             } else {
                 arr[i] = 0;
             }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    //2. Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100;
+    private static void arrHundred() {
+        int [] arr = new int[100];
+        for (int i = 0; i < 100; i++) {
+            arr[i] = i+1;
         }
         System.out.println(Arrays.toString(arr));
     }
