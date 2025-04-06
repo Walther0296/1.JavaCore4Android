a4bd3e4eba1a0f338786fcfbdb6b7ef523f60b2f
Walther0296 <sau100@inbox.ru>
2025-04-06 13:21:26 +0300

    HW2 Task1

--- src/main/l2/BasicConstruction/Main.java
+++ src/main/l2/BasicConstruction/Main.java
@@ -2,7 +2,17 @@ package main.l2.BasicConstruction;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        // check();
+    }
 
+    // 1. Написать метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит в пределах от 10 до 20 (
+    //   включительно), если да – вернуть true, в противном случае – false.
+
+
+    public static void check() {
+        int a = 5;
+        int b = 2;
+        int sum = a + b;
+        System.out.println(sum <= 20 & sum >= 10);
     }
 }
