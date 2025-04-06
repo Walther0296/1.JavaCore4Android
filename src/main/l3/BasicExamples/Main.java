2fcba4441a111fb0820a5a3a052fe750894aa811
Walther0296 <sau100@inbox.ru>
2025-04-06 16:45:42 +0300

    HW3 Task1

---
+++ src/main/l3/BasicExamples/Main.java
@@ -0,0 +1,24 @@
+package main.l3.BasicExamples;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        changeArray();
+    }
+
+    // 1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью
+    //   цикла и условия заменить 0 на 1, 1 на 0;
+    private static void changeArray() {
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < 10; i++) {
+            if (arr[i] == 0) {
+                arr[i] = 1;
+            } else {
+                arr[i] = 0;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
