4de91a476867448278b47c1e9898d0fe9de08082
Walther0296 <sau100@inbox.ru>
2025-04-19 13:27:50 +0300

    Final Commit

--- src/main/l6/OOP2/Main.java
+++ src/main/l6/OOP2/Main.java
@@ -6,16 +6,16 @@ public class Main {
         Cat cat = new Cat("Барсик");
         Dog dog = new Dog("Тошка");
         Dog dog2 = new Dog("Дружок");
+        Dog barbos = new Dog("Барбос");
 
         cat.run(170);
         dog2.run(480);
         cat.swim(30);
         dog2.swim(4);
 
-
-        System.out.println(Dog.getCount());
-        System.out.println(Animal.getCount());
-        System.out.println(Cat.getCount());
+        System.out.println("Животных создано: " + Animal.getCount());
+        System.out.println("Собак создано: " + Dog.getCount());
+        System.out.println("Кошек создано: " + Cat.getCount());
 
     }
 }
